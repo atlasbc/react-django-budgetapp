d2366176ab01aef41ed99038704452523de244da
atlasbc <burakkcivan@gmail.com>
2021-02-11 18:59:50 +0300

    navigation links depends on authentication

--- budget/frontend/src/components/Header.js
+++ budget/frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, {useContext} from "react";
-import { AppBar, Toolbar, IconButton, Button, Link } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Button, Link, Box } from '@material-ui/core';
 // import { MenuIcon } from '@material-ui/icons';
 import MenuIcon from '@material-ui/icons/Menu';
 import Logout from './Logout';
@@ -13,6 +13,10 @@ export default function Header() {
     const  {user}  = useContext(UserContext);
     console.log(`user from Header component ${user}`);
 
+    // Some links must appear only if the user is authenticated TODO 
+    //const dynamic_links = 
+    
+
     return (
 
             <AppBar position="static" style={{marginBottom:"1rem"}}>
@@ -21,16 +25,23 @@ export default function Header() {
                     <MenuIcon/>
                 </IconButton>
                 <nav>
+                {user &&
+                <>
                 <Link component={RouterLink} to="/" color="inherit" style={{marginRight: "12px"}} >Home</Link>
                 <Link component={RouterLink} to="income" color="inherit" style={{marginRight: "12px"}} >Income</Link>
                 <Link component={RouterLink} to="transactions" color="inherit" style={{marginRight: "12px"}} >Transactions</Link>
                 <Link component={RouterLink} to="budget" color="inherit" style={{marginRight: "12px"}} >Budget</Link>
+                </>
+                }
+
                 <Link component={RouterLink} to="about" color="inherit" >About</Link>
 
-                <Button variant="outlined" size="small" color="inherit" style={{margin: "0 12px"}} >
+                {user?<Logout />
+                :<Button variant="outlined" size="small" color="inherit" style={{margin: "0 12px"}} >
                     <Link component={RouterLink} to="login" color="inherit" >Login</Link>
                 </Button>
-                <Logout />
+                }
+
                 {user? user: ""}
                 </nav>
                 </Toolbar>              
