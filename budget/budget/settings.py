4e027ae3d9c31d553cd73b48b7bc86b8c095e941
atlasbc <burakkcivan@gmail.com>
2021-03-18 17:05:37 +0300

    change SECRET_KEY and hide it with environment variables

--- budget/budget/settings.py
+++ budget/budget/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7yyz5v-%kf4n3qiela=a=&4e*y35pr)9rjy=sq=bhxf#jgeih%'
+SECRET_KEY = os.environ.get('BUDGET_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
