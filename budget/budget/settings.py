bcaaf9f5dcf52aa755fbabf5255e4d02e06d2722
atlasbc <burakkcivan@gmail.com>
2021-03-18 18:50:16 +0300

    add required files for heroku deployment, Procfile, runtime.txt. Install gunicorn, and django-heroku.

--- budget/budget/settings.py
+++ budget/budget/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,6 +85,8 @@ DATABASES = {
     }
 }
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
