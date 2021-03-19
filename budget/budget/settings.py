7b5089b61e52e0abaae69d77cc87ac3e86f712d7
atlasbc <burakkcivan@gmail.com>
2021-03-19 20:25:26 +0300

    make debug value dynamic according to environment

--- budget/budget/settings.py
+++ budget/budget/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('BUDGET_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.environ.get('BUDGET_DEBUG_VALUE') == 'TRUE')
 
 ALLOWED_HOSTS = []
 
